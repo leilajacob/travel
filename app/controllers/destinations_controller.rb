719725f46682bdbed3ed15dc42659ed9d2efd15c
Leila Jacob <leilajacob@me.com>
2015-10-28 10:16:45 -0400

    added search function

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -4,7 +4,13 @@ class DestinationsController < ApplicationController
   # GET /destinations
   # GET /destinations.json
   def index
-    @destinations = Destination.all
+    @my_countries = Destination.select(:country).order(:country).distinct
+    
+    if params[:country] == nil
+      @destinations = Destination.all
+    else
+      @destinations = Destination.where(country: params[:country].titleize)
+    end
   end
 
   # GET /destinations/1
