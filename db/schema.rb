122cdb82b1694091fd43128097e4039943d3d928
Leila Jacob <leilajacob@me.com>
2015-10-28 11:06:46 -0400

    added paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151028122841) do
+ActiveRecord::Schema.define(version: 20151028150504) do
 
   create_table "destinations", force: :cascade do |t|
     t.string   "city"
     t.string   "country"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.integer  "population"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
